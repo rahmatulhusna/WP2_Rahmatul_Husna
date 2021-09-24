b2b0c2735e2357a57a55075432d2869063c505cd
rahmatulhusna <rhusna377@gmail.com>
2021-09-24 15:35:39 +0700

    Tugas

---
+++ application/controllers/ContohSatu.php
@@ -0,0 +1,9 @@
+<?php
+class Contohsatu extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>"
+        echo "Nama saya Imam Nawawi Saya tinggal di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis";
+    }
+}
---
+++ application/controllers/Contohaja.php
@@ -0,0 +1,15 @@
+<?php
+class Contohaja extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> BIODATA DIRI </h1>";
+        echo "NIM : 19200894<br>";
+        echo "Nama : Rahmatul Husna<br>";
+        echo "Kelas : 19.3B.05<br>";
+        echo "Program Studi Sistem Informasi<br>";
+        echo "Fakultas Teknik dan Informatika<br>";
+        echo "Universitas Bina Sarana Informatika<br>";
+        echo "BEKASI";
+    }
+}
