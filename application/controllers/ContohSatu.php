6e94ef2243daaf6348ebe8b175c32cc6c6a028d1
rahmatulhusna <rhusna377@gmail.com>
2021-09-24 16:40:11 +0700

    Tugas

--- application/controllers/ContohSatu.php
+++ application/controllers/ContohSatu.php
@@ -4,6 +4,8 @@ class Contohsatu extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalan</h1>"
-        echo "Nama saya Imam Nawawi Saya tinggal di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis";
+        echo "Nama saya Imam Nawawi 
+            Saya tinggal di daerah Ciputat 
+            olah raga yang saya sukai adalah Bulutangkis";
     }
 }
