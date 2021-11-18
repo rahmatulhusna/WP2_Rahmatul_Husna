a1a96521707b0d3605b5eb1be851bfe9ce4c2e13
rahmatulhusna <rhusna377@gmail.com>
2021-11-18 09:50:38 +0700

    Tugas Pertemuan 10

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -1,8 +1,15 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Buku extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+    }
 
+    //manajemen kategori
     public function kategori()
         {
             $data['judul'] = 'Kategori Buku';
